5e18aa2ddd964b86491364123453414ea1c2479e
Colleen Greene <colleen.e.greene@gmail.com>
2013-07-23 18:16:49 -0700

    Cleaned up white space.

--- twentytwelve-child-main/single.php
+++ twentytwelve-child-main/single.php
@@ -14,14 +14,8 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
+
 				<?php get_template_part( 'content', get_post_format() ); ?>
-
-
-
-
-
-
-
 
 
 				<nav class="nav-single">

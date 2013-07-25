e6c3d4bd5164c7614ddb25354b25f7f1545a733f
Colleen Greene <colleen.e.greene@gmail.com>
2013-07-24 19:50:36 -0700

    Swapped library into into footer.

---
+++ twentytwelve-child-main/footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+
+	<strong><center>(c) 2008-2013 by the Pollak Library | <a href="/wp-login.php">Wordpress Login</a></center></strong>
+
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>

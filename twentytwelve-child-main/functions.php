1f234c91c2e9a553258886d7e627eed5b07b690f
Colleen Greene <colleen.e.greene@gmail.com>
2013-07-24 18:00:36 -0700

    Removed User Info, Removed ShareThis on Excerpts.

--- twentytwelve-child-main/functions.php
+++ twentytwelve-child-main/functions.php
@@ -1,9 +1,11 @@
+
+
 <?php
 
 //Add in your custom code below here
 
-<!-- Controls the size of the header -->
-<!-- Test if currently using, since not using the regular header function in header.php -->
+//Controls the size of the header 
+//Test if currently using, since not using the regular header function in header.php 
 function twentytwelvechild_custom_header_setup() {
 	$header_args = array( 'height' => 122, 'width' => 985 );
 	add_theme_support( 'custom-header', $header_args );
@@ -11,20 +13,6 @@ function twentytwelvechild_custom_header_setup() {
 add_action( 'after_setup_theme', 'twentytwelvechild_custom_header_setup' );
 
 
-function wptuts_contact_methods( $contactmethods ) {
- 
-    // Remove we what we don't want
-    unset( $contactmethods[ 'aim' ] );
-    unset( $contactmethods[ 'yim' ] );
-    unset( $contactmethods[ 'jabber' ] );
- 
-    // Add some useful ones
-    $contactmethods[ 'twitter' ] = 'Twitter Username';
-    $contactmethods[ 'facebook' ] = 'Facebook Profile URL';
-    $contactmethods[ 'linkedin' ] = 'LinkedIn Public Profile URL';
-    $contactmethods[ 'googleplus' ] = 'Google+ Profile URL';
- 
-    return $contactmethods;
-}
- 
-add_filter( 'user_contactmethods', 'wptuts_contact_methods' );
+//Removes ShareThis from Excerpts
+remove_filter( 'get_the_excerpt', 'st_remove_st_add_link', 9 );
+remove_filter( 'the_excerpt', 'st_add_widget' );

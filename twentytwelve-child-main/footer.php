5b289a79bf09723f57c4e05a011454a5beb659ff
Colleen Greene <colleen.e.greene@gmail.com>
2013-07-26 10:53:34 -0700

    Modified footer text.

--- twentytwelve-child-main/footer.php
+++ twentytwelve-child-main/footer.php
@@ -14,7 +14,7 @@
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
 
-	<center>(c) 2008-2013 by the Pollak Library | <a href="/wp-login.php">Wordpress Login</a></center>
+	<center>(c) 2008-2013 by the Pollak Library | Powered by a customized version of Wordpress Twenty Twelve | <a href="/wp-login.php">Blog Login</a></center>
 
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->

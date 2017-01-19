21f80673d4487a5d2c4be6eaff4613102dfb3d4d
Colleen Greene <colleen.e.greene@gmail.com>
2017-01-19 13:11:18 -0800

    Added smtpEmailGist.php
    Snippet to functions.php in theme.

--- twentytwelve-child/functions.php
+++ twentytwelve-child/functions.php
@@ -75,3 +75,53 @@ function mytheme_dequeue_styles() {
 wp_enqueue_style( 'mytheme-ie', get_stylesheet_directory_uri() . '/css/ie.css', array( 'twentytwelve-style' ), '1.0' );
 $wp_styles->add_data( 'mytheme-ie', 'conditional', 'lt IE 9' );
 
+
+
+/** 
+* This function will connect wp_mail to your authenticated 
+* SMTP server. This improves reliability of wp_mail, and  
+* avoids many potential problems. 
+* 
+* Author:     Chad Butler 
+* Author URI: http://butlerblog.com 
+* 
+* For more information and instructions, see: 
+ * http://b.utler.co/Y3 
+ */ 
+add_action( 'phpmailer_init', 'send_smtp_email' ); 
+function send_smtp_email( $phpmailer ) { 
+
+
+	// Define that we are sending with SMTP 
+	$phpmailer->isSMTP(); 
+
+
+	// The hostname of the mail server 
+	$phpmailer->Host = "mail.fullerton.edu"; 
+
+
+	// Use SMTP authentication (true|false) 
+	$phpmailer->SMTPAuth = false; 
+
+
+	// SMTP port number - likely to be 25, 465 or 587 
+	$phpmailer->Port = "587"; 
+
+
+	// Username to use for SMTP authentication 
+	//$phpmailer->Username = "yourusername"; 
+
+
+	// Password to use for SMTP authentication 
+	//$phpmailer->Password = "yourpassword"; 
+
+
+	// Encryption system to use - ssl or tls 
+	$phpmailer->SMTPSecure = "tls"; 
+
+
+	$phpmailer->From = "wordpress@libraryblogs.fullerton.edu"; 
+	$phpmailer->FromName = "WordPress "; 
+} 
+
+
